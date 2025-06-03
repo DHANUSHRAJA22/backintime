DHANUSHRAJA22/backintime

commit 1ecedfa5016d597463c2cd80c5c12ba5a4665e83

fix: Use SSH key password only if it is valid, otherwise request it from user (#1852)
Reset cached SSH password when changed. Check if it's valid before using it.
If it's not valid, the user is requested for a new password.

Fixes #1852

Thank you to David Wales @daviewales for this contribution.

---------

Co-authored-by: buhtz <c.buhtz@posteo.jp>

common/sshtools.py
@@ -27,6 +27,7 @@ from pathlib import Path
 from time import sleep
 import logger
 import tools
+import password
 import password_ipc
 from mount import MountControl
 from exceptions import MountException, NoPubKeyLogin, KnownHost
@@ -381,7 +382,7 @@ class SSH(MountControl):
             password_available = any([
                 self.config.passwordSave(self.profile_id),
                 self.config.passwordUseCache(self.profile_id),
-                not self.password is None
+                self.password is not None
             ])
 
             logger.debug('Password available: %s' % password_available, self)
@@ -411,6 +412,33 @@ class SSH(MountControl):
                     thread = password_ipc.TempPasswordThread(self.password)
                     env['ASKPASS_TEMP'] = thread.temp_file
                     thread.start()
+
+                # We need to validate the ssh key password which
+                # `backintime-askpass` will provide before calling
+                # ssh-add below. See Issue #1852.
+
+                # Validate cached SSH key password:
+                proc = subprocess.run(
+                        ['ssh-keygen', '-y', '-f', self.private_key_file],
+                        capture_output=True,
+                        # Ensure ssh-keygen uses backintime-askpass
+                        env=env | {'SSH_ASKPASS_REQUIRE': 'prefer'}
+                )
+
+                # if backintime-askpass supplied an invalid cached password
+                if proc.returncode > 0:
+                    pw = password.Password()
+
+                    # pw_id = 1 corresponds to the SSH password
+                    # (as opposed to the encryption password)
+                    # See qt/settingsdialog.py:SettingsDialog.updateProfile()
+                    pw.password(
+                        None,
+                        self.profile_id,
+                        self.mode,
+                        pw_id=1,
+                        refresh=True,
+                    )
 
                 proc = subprocess.Popen(['ssh-add', self.private_key_file],
                                         stdin=subprocess.PIPE,

common/password.py
@@ -157,10 +157,12 @@ class Password_Cache(daemon.Daemon):
         self.fifo.delfifo()
         super(Password_Cache, self).cleanupHandler(signum, frame)
 
+
 class Password:
     """Provide passwords for BIT either from keyring, Password_Cache or
     by asking user.
     """
+
     def __init__(self, cfg=None):
         self.config = cfg
 
@@ -173,9 +175,15 @@ class Password:
 
         self.keyringSupported = tools.keyringSupported()
 
-    def password(self, parent, profile_id, mode, pw_id=1, only_from_keyring=False):
+    def password(self,
+                 parent,
+                 profile_id,
+                 mode,
+                 pw_id=1,
+                 only_from_keyring=False,
+                 refresh=False):
         """
-        based on profile settings return password from keyring,
+        Based on profile settings return password from keyring,
         Password_Cache or by asking User.
         """
         if not self.config.modeNeedPassword(mode, pw_id):
@@ -186,32 +194,34 @@ class Password:
         user_name = self.config.keyringUserName(profile_id)
 
         try:
-            return self.db['%s/%s' %(service_name, user_name)]
+            return self.db['%s/%s' % (service_name, user_name)]
 
         except KeyError:
             pass
 
         password = ''
 
-        if self.config.passwordUseCache(profile_id) and not only_from_keyring:
+        if (self.config.passwordUseCache(profile_id)
+                and not only_from_keyring
+                and not refresh):
             # From cache
             password = self.passwordFromCache(service_name, user_name)
 
-            if not password is None:
+            if password is not None:
                 self.setPasswordDb(service_name, user_name, password)
 
                 return password
 
-        if self.config.passwordSave(profile_id):
+        if self.config.passwordSave(profile_id) and not refresh:
             # From keyring
             password = self.passwordFromKeyring(service_name, user_name)
 
-            if not password is None:
+            if password is not None:
                 self.setPasswordDb(service_name, user_name, password)
 
                 return password
 
-        if not only_from_keyring:
+        if refresh or not only_from_keyring:
             # Ask user and write to cache
             password = self.passwordFromUser(parent, profile_id, mode, pw_id)
 

commit cc99913265590096b059b702241aa02fa7bec112

fix: Unlocking SSHkey with passphrase on new created SSH profiles (#2163)
A change (1ecedfa) was made to run ssh-keygen with 'SSH_ASKPASS_REQUIRE': 'prefer'.
However, this used up the password placed in the FIFO that was put there for the use in the following invocation of ssh-add.
This caused the ssh-add to not have a waiting password in the FIFO, resulting in a failure.

This commit fix this problem with filling of again the FIFO after the ssh-keygen check.

Bug introduced with 1ecedfa and released in v1.5.4.
Thank you to @davidfjoh for analyzing the bug and providing this fix.
Keeping #2164 open until the fix arrived in hotfix release v1.5.5.
---------
Co-authored-by: Christian Buhtz <c.buhtz@posteo.jp>

common/sshtools.py
@@ -437,6 +437,13 @@ class SSH(MountControl):
                         refresh=True,
                     )
 
+                if self.password:
+                    thread.stop()
+                    logger.debug('Provide password through temp FIFO', self)
+                    thread = password_ipc.TempPasswordThread(self.password)
+                    env['ASKPASS_TEMP'] = thread.temp_file
+                    thread.start()
+
                 proc = subprocess.Popen(['ssh-add', self.private_key_file],
                                         stdin=subprocess.PIPE,
                                         stdout=subprocess.PIPE,